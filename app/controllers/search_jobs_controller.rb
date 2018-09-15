4e55a88a0a1da215b62678651e4fa51fcb0b6e03
ran010 <ranjanbajra@gmail.com>
2018-09-15 13:08:41 +0545

    a

--- app/controllers/search_jobs_controller.rb
+++ app/controllers/search_jobs_controller.rb
@@ -1,11 +1,12 @@
 class SearchJobsController < ApplicationController
+  
   def index
     if params[:search].blank?
       @job_posts = JobPost.order("created_at DESC").paginate(:page => params[:page],
                                                              :per_page => 10)
     else
       @job_posts = JobPost.fuzzy_search(params[:search]).paginate(:page => params[:page],
-                                                                  :per_page => 10) 
+                                                                  :per_page => 10)
     end
   end
 end
